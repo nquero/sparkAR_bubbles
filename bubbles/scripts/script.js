65f30ba3b73b116faf5e2de85461740e36eb6d51
robsouthgate4 <robsouthgate4@gmail.com>
2019-05-08 10:58:07 +0100

    Added bubble object tap

---
+++ bubbles/scripts/script.js
@@ -0,0 +1,22 @@
+
+/**
+ * Scripting 101: Particle scripting
+ */
+
+const Diagnostics = require('Diagnostics')
+const Scene = require('Scene')
+const Particles = require('Particles')
+const TouchGestures = require('TouchGestures')
+
+
+
+for (let i = 1; i < 8; i++) {
+
+    // const bubble = Scene.root.find(`bubbles_${i}`).find('plane0')
+
+    // TouchGestures.onTap(bubble).subscribe((e) => {
+    //     Diagnostics.log(bubble)
+    // })
+    
+  
+}
